3909867e09d93b4522ad839d474059a5063983d8
Kevin Zhang <kzhaaang@gmail.com>
2018-01-05 18:59:37 -0500

    more deps

--- bot.js
+++ bot.js
@@ -1,39 +1,40 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
-    colorize: true
-});
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
-});
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
+// Import the discord.js module
+const discord = require('discord.js')
+const auth = require('./auth.json')
 
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
-            // Just add any case commands if you want to..
-         }
-     }
+// Create an instance of Discord that we will use to control the bot
+const bot = new Discord.Client();
+
+// Token for your bot, located in the Discord application console - https://discordapp.com/developers/applications/me/
+const token = auth.token;
+
+// Gets called when our bot is successfully logged in and connected
+bot.on('ready', () => {
+    console.log('Hello World!');
 });
+
+// Event to listen to messages sent to the server where the bot is located
+bot.on('message', message => {
+    // So the bot doesn't reply to iteself
+    if (message.author.bot) return;
+
+    // Check if the message starts with the `!` trigger
+    if (message.content.indexOf('!') === 0) {
+        // Get the user's message excluding the `!`
+        var text = message.content.substring(1);
+
+        // Reverse the message
+        var reversed = '';
+        var i = text.length;
+
+        while (i > 0) {
+            reversed += text.substring(i - 1, i);
+            i--;
+        }
+
+        // Reply to the user's message
+        message.reply(reversed);
+    }
+});
+
+bot.login(token);
