255fe1862053bd1fb9daeeb4e6aea73eea1ce3d2
Kevin Zhang <kzhaaang@gmail.com>
2018-01-05 20:07:27 -0500

    command -> meme

--- bot.js
+++ bot.js
@@ -22,46 +22,46 @@ bot.on("message", async message => {
         await message.react("398962296967397376")
         await message.react("398962692347396099")
     } else {
-        // custom commands
+        // custom memes, borrowed heavily from https://github.com/iArePJ/CommandBot
         let checkMessage = message.content.split(" ");
-        if (checkMessage[0] === '~createcommand') {
+        if (checkMessage[0] === '^creatememe') {
             try {
-                let commandName = checkMessage[1];
-                if (commandName === '~commands' || commandName === '~help') {
+                let memeName = checkMessage[1];
+                if (memeName === '^memes' || memeName === '^help') {
                     message.channel.send("You can't do that.");
                     return null;
                 }
-                let commandText = message.content.split('|', 2);
-                if (commandText[1] === undefined) {
+                let memeText = message.content.split('|', 2);
+                if (memeText[1] === undefined) {
                     message.channel.send("You forgot to use '|'");
                     return null;
                 }
-                if (commandName.charAt(0) === '~') {
-                    checkExistingCommand(commandText[1], commandName);
+                if (memeName.charAt(0) === '^') {
+                    checkExistingMeme(memeText[1], memeName);
                 } else {
-                    checkExistingCommand(commandText[1], '~' + commandName);
+                    checkExistingMeme(memeText[1], '^' + memeName);
                 }
-                message.channel.send("Command " + commandName + " has been created.");
+                message.channel.send("Meme " + memeName + " has been created.");
 
             } catch (error) {
                 console.log("Error\nAuthor: " + message.author.username + "\nMessage: " + message.content);
             }
         }
 
-        fs.readFile('./commands/commands.txt', 'utf8', function (err, f) {
+        fs.readFile('./memes/memes.txt', 'utf8', function (err, f) {
             let com = f.toString().split(";");
             for (i = 0; i < com.length; i++) {
                 if (message.content === com[i]) {
-                    if (com[i] === "~commands") {
+                    if (com[i] === "^memes") {
                         message.author.send(com);
                         break;
                     }
-                    if (com[i] === "~help") {
-                        message.channel.send("How to create commands:\n~createcommand ~NameOfCommand | Type whatever you want here");
+                    if (com[i] === "^help") {
+                        message.channel.send("How to create memes:\n^creatememe ^NameOfMeme | Type whatever you want here");
                         break;
                     }
-                    let command = "./commands/" + com[i] + ".txt";
-                    fs.readFile(command, 'utf8', function (err, f) {
+                    let meme = "./memes/" + com[i] + ".txt";
+                    fs.readFile(meme, 'utf8', function (err, f) {
                         try {
                             let com2 = f.toString().split(";");
                             let num = random.integer(0, com2.length - 1);
@@ -77,38 +77,38 @@ bot.on("message", async message => {
     }
 });
 
-function checkExistingCommand(commandText, commandName) {
-    let commandExists = false;
-    fs.readFile('./commands/commands.txt', 'utf8', function (err, f) {
-        let findCommands = f.toString().split(";");
-        for (i = 0; i < findCommands.length; i++) {
-            if (commandName === findCommands[i]) {
-                commandExists = true;
+function checkExistingMeme(memeText, memeName) {
+    let memeExists = false;
+    fs.readFile('./memes/memes.txt', 'utf8', function (err, f) {
+        let findMemes = f.toString().split(";");
+        for (i = 0; i < findMemes.length; i++) {
+            if (memeName === findMemes[i]) {
+                memeExists = true;
             }
         }
-        if (commandExists === true) {
-            createCommand(commandText, true, commandName);
-        } else if (commandExists === false) {
-            createCommand(commandText, false, commandName);
+        if (memeExists === true) {
+            createMeme(memeText, true, memeName);
+        } else if (memeExists === false) {
+            createMeme(memeText, false, memeName);
         }
     });
 
 }
 
-function createCommand(commandText, commandExists, commandName) {
-    let fileName = "./commands/" + commandName + ".txt";
-    if (commandExists === true) {
-        fs.writeFile(fileName, commandText, function (err) {
+function createMeme(memeText, memeExists, memeName) {
+    let fileName = "./memes/" + memeName + ".txt";
+    if (memeExists === true) {
+        fs.writeFile(fileName, memeText, function (err) {
             if (err) {
                 return console.error(err);
             }
         });
-    } else if (commandExists === false) {
-        fs.appendFile('./commands/commands.txt', commandName + ';', (err) => {
+    } else if (memeExists === false) {
+        fs.appendFile('./memes/memes.txt', memeName + ';', (err) => {
             if (err) throw err;
         });
 
-        fs.writeFile(fileName, commandText.trim(), function (err) {
+        fs.writeFile(fileName, memeText.trim(), function (err) {
             if (err) {
                 return console.error(err);
             }
