9864dad271b12b26fb563b780fcbee5248b6e76b
Kevin Zhang <kzhaaang@gmail.com>
2018-01-05 19:40:47 -0500

    just in case anyone wants to read this pos

--- bot.js
+++ bot.js
@@ -1,22 +1,26 @@
 const discord = require("discord.js")
 const auth = require("./auth.json")
-
 const bot = new discord.Client();
 
 // auth.json shouldn't be public
 const token = auth.token;
 
+// explosions
 bot.on("ready", () => {
     console.log("我が名はめぐみん！");
 });
 
+// asynchronously respond and react to messages
 bot.on("message", async message => {
+    // obviously don't want to infinite combo
     if(message.author.bot) return;
 
+    // standard poe prefix
     if(message.content.startsWith("Rarity: Rare")) {
         await message.react("398962296967397376")
         await message.react("398962692347396099")
     }
 });
 
+// hack into the database B)
 bot.login(token);
